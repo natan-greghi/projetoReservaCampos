68d5a30831bf043655ec88050bf0b82d4173b831
natan-greghi <natan.greghi@gmail.com>
2015-10-28 15:18:30 -0200

    arrumando a pesquisa

--- application/models/principal_model.php
+++ application/models/principal_model.php
@@ -6,18 +6,16 @@ class Principal_model extends CI_Model {
 
 	public function consulta($dados = null)
 	{
+
 		if($dados != null){
-			$this->db->select('h.*, t.nome AS "tipoquarto",r.*');
+			$this->db->select('h.*, t.nome AS "tipoquarto"');
 			$this->db->select_min('q.precoFixo');
 			$this->db->select_min('q.precoPromo');
 			$this->db->from('hotelpousada h');
 			$this->db->join('quartos q', 'h.idHotelPousada = q.hotelPousada_idHotelPousada');
 			$this->db->join('tipoquarto t','q.tipoQuarto_idtipoQuarto = t.idtipoQuarto');
-			$this->db->join('reservas r', 'r.quartos_idquartos = q.idquartos');
 			$this->db->where('t.idtipoQuarto',$dados['tipoQuarto']);
-			$this->db->where('r.dataInicio !=',$dados['dataInicio']);
-			$this->db->where('r.dataFim !=',$dados['dataFim']);
-			$this->db->like('h.nome',$dados['nome']);
+			$this->db->or_like('h.nome',$dados['nome']);
 			$this->db->group_by('h.nome');
 			$this->db->order_by('q.precoFixo','asc');
 			$query = $this->db->get();
